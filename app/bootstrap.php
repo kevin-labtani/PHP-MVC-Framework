18ce394d98bca0c54fdc102f4beeed4b3d3ec6a5
Kevin Labtani <kevinlabtani@gmail.com>
2020-10-25 15:34:45 +0100

    bootstrap our libraries and Core class to get url params

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,1 +1,6 @@
-hello
+<?php
+
+    // load libraries
+    require_once 'libraries/Core.php';
+    require_once 'libraries/Controller.php';
+    require_once 'libraries/Database.php';
---
+++ app/libraries/Core.php
@@ -0,0 +1,21 @@
+<?php
+
+    // Core Class
+    // Create URL and loads core controller
+    // URL Format - /controller/method/params
+    class Core
+    {
+        protected $currentController = 'Pages';
+        protected $currentMethod = 'index';
+        protected $params = [];
+
+        public function __construct()
+        {
+            $this->getUrl();
+        }
+
+        public function getUrl()
+        {
+            echo $_GET['url'];
+        }
+    }
