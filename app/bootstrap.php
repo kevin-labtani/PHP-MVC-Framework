a419d67ed484ed0c0e22b3943f10b0787a7bf71f
Kevin Labtani <kevinlabtani@gmail.com>
2020-10-25 17:25:43 +0100

    auto loader for Core Libraries

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,7 +3,7 @@
     // Load config
     require_once 'config/config.php';
 
-    // load libraries
-    require_once 'libraries/Core.php';
-    require_once 'libraries/Controller.php';
-    require_once 'libraries/Database.php';
+    // auto loader for Core Libraries
+    spl_autoload_register(function ($className) {
+        require_once "libraries/{$className}.php";
+    });
